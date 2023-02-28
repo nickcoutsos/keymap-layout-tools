c6b3779ca8448808f5a4a57614f8f79c90e68d60
Nick Coutsos <nick@coutsos.com>
2023-02-28 00:13:09 -0500

    Simplify "padding" in key calculations

--- keymap-layout-tools/lib/geometry.js
+++ keymap-layout-tools/lib/geometry.js
@@ -1,16 +1,16 @@
 import map from 'lodash/map'
 
-const DEFAULT_SIZE = 65
+const DEFAULT_SIZE = 70
 const DEFAULT_PADDING = 5
 
 export function getComputedParams (position, size, rotation = {}) {
   return {
-    x: position.x * (DEFAULT_SIZE + DEFAULT_PADDING),
-    y: position.y * (DEFAULT_SIZE + DEFAULT_PADDING),
-    u: size.u * DEFAULT_SIZE + DEFAULT_PADDING * (size.u - 1),
-    h: size.h * DEFAULT_SIZE + DEFAULT_PADDING * (size.h - 1),
-    rx: (position.x - (rotation.x ?? position.x)) * -(DEFAULT_SIZE + DEFAULT_PADDING),
-    ry: (position.y - (rotation.y ?? position.y)) * -(DEFAULT_SIZE + DEFAULT_PADDING),
+    x: position.x * DEFAULT_SIZE,
+    y: position.y * DEFAULT_SIZE,
+    u: size.u * DEFAULT_SIZE - DEFAULT_PADDING,
+    h: size.h * DEFAULT_SIZE - DEFAULT_PADDING,
+    rx: (position.x - (rotation.x ?? position.x)) * DEFAULT_SIZE,
+    ry: (position.y - (rotation.y ?? position.y)) * DEFAULT_SIZE,
     a: rotation.a || 0
   }
 }
