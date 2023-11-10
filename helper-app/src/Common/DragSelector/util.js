af1ab2dbdac4f52e0da6d6dbeedc29d84bb86e15
Nick Coutsos <nick@coutsos.com>
2023-11-10 17:51:22 -0500

    Add support code for polygon-polygon intersections

--- helper-app/src/Common/DragSelector/util.js
+++ helper-app/src/Common/DragSelector/util.js
@@ -48,6 +48,8 @@ export function getIntersectingPolygonsTrail (trail, polygons) {
   }, [])
 }
 
+// These should all be moved to keymap-editor-tools/lib/geometry.
+
 function bboxContainsPoint (bbox, { x, y }) {
   return (
     bbox[0][0] < x && x < bbox[1][0] &&
@@ -55,11 +57,32 @@ function bboxContainsPoint (bbox, { x, y }) {
   )
 }
 
-function vec (a, b) {
-  return { x: b.x - a.x, y: b.y - a.y }
+function vec (from, to) {
+  return {
+    x: to.x - from.x,
+    y: to.y - from.y
+  }
+}
+
+function unit (v) {
+  const norm = Math.sqrt(v.x * v.x + v.y * v.y)
+  return {
+    x: v.x / norm,
+    y: v.y / norm
+  }
+}
+
+function perp ({ x, y }) {
+  return {
+    x: y,
+    y: -x
+  }
 }
 function cross (a, b) {
   return a.x * b.y - a.y * b.x
+}
+function dot (a, b) {
+  return (a.x * b.x) + (a.y * b.y)
 }
 
 // https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
@@ -98,4 +121,74 @@ function segmentsFromPoly (poly) {
     [c, d],
     [d, a]
   ]
+}
+
+/**
+ * Check if two convex polygons intersect
+ *
+ * Based on separating-axis-theorem:
+ * https://www.metanetsoftware.com/technique/tutorialA.html#section1
+ *
+ * @param {Array<{x, y}>} polyA four points ordered counter-clockwise
+ * @param {Array<{x, y}>} polyB four points ordered counter-clockwise
+ * @param {Object} [options={}]
+ * @param {boolean} [options.bothAreQuads=false] assert that both polygons are quadrilaterals to simplify the intersection test
+ * @param {number} [options.threshold=0] minimum overlap distance before counting as an intersection
+ * @returns {boolean}
+ */
+export function polygonsIntersect (polyA, polyB, options = {}) {
+  const {
+    bothAreQuads = false,
+    threshold = 0
+  } = options
+
+  const segments = [
+    ...segmentsFromPoly(polyA).slice(0, bothAreQuads ? 2 : undefined),
+    ...segmentsFromPoly(polyB).slice(0, bothAreQuads ? 2 : undefined)
+  ]
+
+  for (const segment of segments) {
+    const axis = unit(perp(vec(...segment)))
+    const aBounds = getProjectedBounds(polyA, axis)
+    const bBounds = getProjectedBounds(polyB, axis)
+    if (getOverlapDistance(aBounds, bBounds) < threshold) {
+      return false
+    }
+  }
+
+  return true
+}
+
+/**
+ * Return the min and max scalar of a set of points projected onto an axis
+ * @param {Array<{x, y}>} points
+ * @param {Vector} axis
+ * @returns {Array<Number>}
+ */
+function getProjectedBounds (points, axis) {
+  return points.reduce(([min, max], point) => {
+    const scalar = dot(point, axis)
+    return [
+      Math.min(min, scalar),
+      Math.max(max, scalar)
+    ]
+  }, [Infinity, -Infinity])
+}
+
+/**
+ * Measure the overlap of two ranges (1-dimensional line segments)
+ * @param {Array<Number>} a
+ * @param {Array<Number>} b
+ * @returns {Number}
+ */
+function getOverlapDistance (a, b) {
+  const [a_, b_] = [a, b].sort(([aMin], [bMin]) => aMin - bMin)
+
+  if (b_[0] > a_[1]) {
+    return 0
+  }
+
+  return b_[1] < a_[1]
+    ? (b_[1] - b_[0])
+    : (a_[1] - b_[0])
 }
